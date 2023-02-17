f9dd10a42babcc83fce8dfe0304be044e312002d
Malonyl-CoA <borikassimu@gmail.com>
2023-02-17 05:04:51 -0800

    print without puts,printf and their altenatives

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - execute program without printf,
+ * and puts with its altenative functions
+ *
+ * Return: Always 1 if no error
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
