eef8b932e9e02a34692a025dbaa661b08bdf933b
Malonyl-CoA <borikassimu@gmail.com>
2023-02-26 04:06:49 -0800

    start of nested loop task

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,16 @@
+##HOW TO USE LOOP STATEMENTS
+
+### WHILE LOOP
+
+- normal while loop
+- nested while loop
+
+### FOR LOOP
+
+- normal for loop
+- nested for loop
+
+### do while
+
+- normal do-while loop
+- nested do-while loop 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,45 @@
+/**
+ * This is how you create a header file.
+ *
+ * A header file is a file with extension .h 
+ * which contains C function declarations and 
+ * macro definitions to be shared between several source files. 
+ */
+
+
+#ifndef main_h 
+
+/**
+ * Putting the #ifndef wrapper around the contents
+ * means the compiler only parses the header's contents once, 
+ * and avoids redefinition errors
+ */
+
+#define main_h
+
+
+int _putchar(char);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+#endif
