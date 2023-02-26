dc016b9ff53f3ecd3d9a03635cf66fa8371279d3
Malonyl-CoA <borikassimu@gmail.com>
2023-02-26 07:54:59 -0800

    function to print alphabrt 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	char k;
+	int b;
+
+	for (b = 1; b <= 10; b++)
+	{
+	for (k = 'a'; k <= 'z'; k++)
+	{
+	_putchar(k);
+	}
+	putchar('\n');
+	}
+}
