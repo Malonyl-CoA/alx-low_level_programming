827ec85c40e6189cc8659449cc76dc73ea5e2cec
Malonyl-CoA <borikassimu@gmail.com>
2023-02-27 11:41:20 -0800

    function print signs if condition true

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function print sign if true
+ *
+ * @n: parameter to be checked
+ *
+ * Return: (1 if n > 0) (0 if n = 0) & (-1 if n < 0)
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+}
