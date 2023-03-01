7048256203c21f822f0dc7b665bba79c168ecb1f
Malonyl-CoA <borikassimu@gmail.com>
2023-03-01 09:25:04 -0800

    function print nth times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,45 +8,44 @@
 
 void print_times_table(int n)
 {
-	int x, y, z, k, m;
+	int x, y, z;
 
+	if (n >= 0 && n <= 14)
+	{
 	for (x = 0; x <= n; x++)
 	{
 	for (y = 0; y <= n; y++)
 	{
 	z = x * y;
-	if (z == 0 && y == 0)
-	_putchar(z + '0');
-	else if (z > 99)
+	if (z > 99)
 	{
-	k = z % 10;
-	m = (z / 10) % 10;
-	z /= 100;
 	_putchar(',');
-	_putchar(' ');
-	_putchar(z + '0');
-	_putchar(m + '0');
-	_putchar(k + '0');
+	_putchar(32);
+	_putchar((z / 100) + '0');
+	_putchar(((z / 10) % 10) + '0');
+	_putchar((z % 10) + '0');
 	}
 	else if (z > 9)
 	{
-	k = z % 10;
-	z /= 10;
 	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(z + '0');
-	_putchar(k + '0');
+	_putchar(32);
+	_putchar(32);
+	_putchar(((z / 10) % 10) + '0');
+	_putchar((z % 10) + '0');
 	}
 	else
 	{
+	if (y != 0)
+	{
 	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
+	_putchar(32);
+	_putchar(32);
+	_putchar(32);
+	}
 	_putchar(z + '0');
 	}
 	}
 	_putchar('\n');
+	}
 	}
 }
