68c1145759c9bc81c5ddbbbe2261e453bf9983dc
Malonyl-CoA <borikassimu@gmail.com>
2023-03-01 05:46:55 -0800

    function add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - function add two numbers
+ *
+ * @x: first parameter
+ * @y: second parameter
+ *
+ * Return: result
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	_putchar(z + '0');
+
+	return (z);
+}
