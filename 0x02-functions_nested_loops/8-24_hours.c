b6a253b1264d9853a096cdb7c8a5325049e742b8
Malonyl-CoA <borikassimu@gmail.com>
2023-03-01 03:41:28 -0800

    function that print 24hrs with minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function print 24 hours with minutes
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+
+{
+	int w, x;
+
+	for (w = 0; w < 25; w++)
+	{
+	for (x = 0; x <= 59; x++)
+	{
+	_putchar(w / 10 + '0');
+	_putchar(w % 10 + '0');
+	_putchar(':');
+	_putchar(x / 10 + '0');
+	_putchar(x % 10 + '0');
+	_putchar('\n');
+	}
+	}
+}
+
