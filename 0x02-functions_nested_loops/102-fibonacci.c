268b8b67f5ff6ef29663fc601bb37e0abe5dbbd9
Malonyl-CoA <borikassimu@gmail.com>
2023-03-01 12:55:43 -0800

    no need to store fib in array it save space

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 fibonnaci numbers
+ * main - print first 50 fibonacci numbers
  *
- * Return: 0 (success)
+ * Retur: 0 (success)
  */
 
 int main(void)
-{
-	int n = 50;
-	int fib[n];
-	int i;
-	
-	fib[0] = 1;
-	fib[1] = 1;
 
-	for (i = 2; i < n; i++)
+{
+	int first = 1, second = 2, next;
+
+	printf("%d, %d, ", first, second);
+
+	for (int i = 3; i <= 50; i++)
 	{
-	fib[i] = fib[i - 1] + fib[i - 2];
-	}
-	for (i = 1; i < n; i++)
-	{
-	printf("%d, ", fib[i]);
+	next = first + second;
+	printf("%d, ", next);
+	first = second;
+	second = next;
 	}
 	printf("\n");
-	return (0);
+	return 0;
 }
