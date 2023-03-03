50bb41a748c0ea7cffa20b45503f31d85683d0bd
Malonyl-CoA <borikassimu@gmail.com>
2023-03-03 11:20:00 -0800

    trailing white space removed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,31 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (a > c && c > b)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
-	else if (b > c && c > b)
-	{
-		largest = b;
-	}
-	else if (c > a && a > b)
-	{
-		largest = c;
-	}
-	else if (c > b && b > c)
+	else
 	{
 		largest = c;
 	}
 
 	return (largest);
 }
-
