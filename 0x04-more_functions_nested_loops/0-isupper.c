c45cd9257b8fa7547009e9432a4cea817a67912a
Malonyl-CoA <borikassimu@gmail.com>
2023-03-04 09:27:09 -0800

    function check uppercase aphabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - print uppercase alphabet
+ *
+ * @c: function parameter to be checked
+ *
+ * Return: 1 if true 0 if false
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' &&  c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
