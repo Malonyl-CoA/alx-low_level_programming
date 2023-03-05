32e2c1797d7570ce4eefd86129e23966a82c183c
Malonyl-CoA <borikassimu@gmail.com>
2023-03-05 04:26:41 -0800

    print Fizz_Buzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints words in place of %3 and %5
+ *
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+	if ((n % 3 == 0) && (n % 5 == 0))
+	{
+		printf("FizzBuzz");
+	}
+	else if (n % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else if (n % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	if (n != 100)
+	{
+		printf(" ");
+	}
+	}
+	printf("\n");
+
+	return (0);
+}
