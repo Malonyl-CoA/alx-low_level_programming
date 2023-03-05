79b377b96f57d06328f80709a8ed1ac855c27bc3
Malonyl-CoA <borikassimu@gmail.com>
2023-03-04 22:23:08 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - the function prints line
+ *
+ * @n: number of times to print
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int k;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (k = 0; k <= n; k++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
